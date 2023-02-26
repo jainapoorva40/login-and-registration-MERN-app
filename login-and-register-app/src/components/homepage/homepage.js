9945b8bf86bb1b6d481d20b47dfd58a073fa3b5e
Apoorva <jainapoorva40@gmail.com>
2023-02-26 17:36:18 +0530

    second

--- login-and-register-app/src/components/homepage/homepage.js
+++ login-and-register-app/src/components/homepage/homepage.js
@@ -5,7 +5,7 @@ const Homepage=({setLoginUser})=>{
     return(
         <div className="homepage">
         <h1>Hello homepagee</h1>
-        <div className="button" onClick={()=>setLoginUser({})}>Logout</div>
+        <div className="button" onClick={()=>setLoginUser({})}>Logouttt</div>
         </div>
     )
 }
